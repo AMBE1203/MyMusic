12858c26a4bf6b9f99f1b99a8acd749366847c7f
duong the cuong <duongthecuongbk227@gmail.com>
2019-06-29 11:08:18 +0700

    first commit

---
+++ app/src/main/java/com/ambe/myappplaymusic/model/MusicItem.kt
@@ -0,0 +1,6 @@
+package com.ambe.myappplaymusic.model
+
+/**
+ *  Created by AMBE on 29/6/2019 at 11:05 AM.
+ */
+data class MusicItem(val mPath: String, val mTitle: String, var mArtist: String, var mDuration: Long)
